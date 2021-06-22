de20b35d3e3d612f69c39e9564608272c8581abb
Jessi Tambascia <jessica.tambascia@gmail.com>
2021-06-22 17:43:51 -0300

    Other

--- JavaScript/contacto.js
+++ JavaScript/contacto.js
@@ -1,6 +1,18 @@
-var formulario
-var Usuario
-var email
-var telefono
-var asunto
-var mensaje
+var formulario;
+var usuario;
+var email;
+var telefono;
+var asunto;
+var mensaje;
+
+function enviar(event){
+    event.preventDefault();
+    usuario = prompt("Ingrese su nombre completo: ");
+    email = prompt("Ingrese su email: ");
+    telefono = prompt("Ingrese su teléfono: ");
+    asunto = prompt("Motivo de su consulta");
+    mensaje = prompt("Ingrese su mensaje");
+
+}
+
+
