76eb3fa4165667af69e81f3c387eb08f3902de33
Jessi Tambascia <jessica.tambascia@gmail.com>
2021-07-06 20:47:10 -0300

    ultimo

--- JavaScript/contacto.js
+++ JavaScript/contacto.js
@@ -1,35 +1,34 @@
-var formulario;
-var usuario;
-var email;
-var telefono;
-var asunto;
-var mensaje;
-var regName = / ^ [a-zA-Z] + [a-zA-Z] + $ /;
-
-
-
-function click(event){
-    event.preventDefault();
-    
+function click2(event){
+    var formulario;
+    var usuario;
+    var email;
+    var telefono;
+    var asunto;
+    var mensaje;
+    var regName = / ^ [a-zA-Z] + [a-zA-Z] + $ /;
+  
     usuario = document.getElementById("usuario").value;
     email = document.getElementById("email").value;
     telefono = document.getElementById("telefono").value;
     asunto = document.getElementById("asunto").value;
     mensaje = document.getElementById("mensaje").value;
-    
-    if (!isNaN (usuario)) {
-        alert("Ingrese un nombre válido");
+  
+  
+  
+    event.preventDefault(); 
+    if(!isNaN(usuario)){
+        window.alert("Ingrese un nombre válido");
       return false;
     }
-    else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email)) {
+    else if (/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3,4})+$/.test(email)) {
       alert('[ERROR] El campo debe contener un email válido');
       return false;
     } 
-    else if ( isNaN(telefono) || !(/^\d{9}$/.test(telefono))){
-    alert('[ERROR] El campo debe contener un teléfono válido conforme el siguiente formato [0-9]{3}-[0-9]{7}');
+    else if ( isNaN(telefono)){
+      alert('[ERROR] El campo debe contener un teléfono válido');
       return false;
     } 
-    alert('Mensaje enviado correctamente'); 
+  alert('Mensaje enviado correctamente'); 
   };
 
 
